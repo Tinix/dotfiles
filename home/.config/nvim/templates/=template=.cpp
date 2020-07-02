dfc778321afe4d1fcd69f89fcba979f605dd9dcd
voldikss <dyzplus@gmail.com>
2020-07-02 19:39:26 +0800

    Update template.cpp

--- home/.config/nvim/templates/=template=.cpp
+++ home/.config/nvim/templates/=template=.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <climits>
 #include <iostream>
+#include <list>
 #include <map>
 #include <queue>
 #include <set>
@@ -10,6 +11,12 @@
 #include <unordered_map>
 #include <vector>
 using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &l) {
+    for (auto &i : l) os << i << " ";
+    return os;
+}
 
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &vec) {
